6586781c3cc88c1dba2b0786a350d304df2a9ff9
Salah Ahmed <riddenariel@gmail.com>
2024-02-20 19:47:58 +0200

    Add button to toggle read status

--- js/script.js
+++ js/script.js
@@ -34,16 +34,24 @@ function displayBooks() {
 		const newPages = document.createElement("p");
 		const newRead = document.createElement("p");
 		const removeButton = document.createElement("button");
+		const readButton = document.createElement("button");
 
+		if (item.read === true) {
+			newRead.textContent = "Read? Yes";
+		} else if (item.read === false) {
+			newRead.textContent = "Read? No";
+		}
+		readButton.textContent = "Read Toggle";
 		removeButton.classList.add("rmv");
 		removeButton.textContent = "Delete";
 		newTitle.textContent = "Title: " + item.title;
 		newAuthor.textContent = "Author: " + item.author;
 		newPages.textContent = "Pages: " + item.pages;
-		newRead.textContent = "Did I read it? " + item.read;
 
+		readButton.style.cssText =
+			"width: 100%; border: none; border-radius: 5px; background-color: #dd7973; color: black; font-weight: bold";
 		removeButton.style.cssText =
-			"width: 50%; align-self: flex-end; justify-self: end; border: none; border-radius: 5px; background-color: #dd7973; color: black; font-weight: bold";
+			"width: 100%; border: none; border-radius: 5px; background-color: #dd7973; color: black; font-weight: bold";
 		newTitle.style.cssText = "color: white; font-style: italic";
 		newAuthor.style.cssText = "color: white; font-style: italic";
 		newPages.style.cssText = "color: white; font-style: italic";
@@ -57,10 +65,18 @@ function displayBooks() {
 		newCard.appendChild(newPages);
 		newCard.appendChild(newRead);
 		newCard.appendChild(removeButton);
+		newCard.appendChild(readButton);
 
 		// Append newCard to showcase
 		showcase.appendChild(newCard);
 
+		readButton.addEventListener("click", () => {
+			if (newRead.textContent === "Read? Yes") {
+				newRead.textContent = "Read? No";
+			} else if (newRead.textContent === "Read? No") {
+				newRead.textContent = "Read? Yes";
+			}
+		});
 		removeButton.addEventListener("click", () => {
 			myLibrary.splice(myLibrary.indexOf(item), 1);
 			const parentElement = removeButton.parentNode;
